f26498f9f1c3d49db84b85736d1059bc0c8a7933
Davinder Singh <davindersaini003@gmail.com>
2020-04-09 19:48:08 +0530

    tailwind

---
+++ TS/tailwind-design/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'brand-blue': '#5a67d8',
+      },
+      spacing: {
+        '72': '18rem',
+      }
+    },
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active']
+  },
+  plugins: [],
+}
